659c5f45390846c44b7b546c988fb69f2d03df37
version7x <32801437+version7x@users.noreply.github.com>
2020-06-25 17:10:57 -0400

    Update homebrew-install.sh

--- bin/homebrew-install.sh
+++ bin/homebrew-install.sh
@@ -1,4 +1,7 @@
+xcode-select --install
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+brew install git
 brew install python3
 brew install pip3
