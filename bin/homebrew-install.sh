63add5312df91314a3398915804d769963d110fa
version7x <32801437+version7x@users.noreply.github.com>
2020-06-26 08:04:45 -0400

    added setup commands

--- bin/homebrew-install.sh
+++ bin/homebrew-install.sh
@@ -1,9 +1,25 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                        
+
+chsh -s /bin/bash 2>/dev/null
 
 xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install git
+brew install git 
+git clone https://github.com/version7x/home.git 
+cp -Rp home/* .
+cp -Rp home/.bash_profile $HOME/
+cp -Rp home/.vimrc $HOME/
+# Clean Up
+rm -rf home
+rm -rf windows-terminal.json
+
+. ~/.bash_profile
+
+# Install dev pkgs
 brew install python3
-brew install pip3
+pip3 install --upgrade pip --user
+
+brew install terraform
+brew install docker
