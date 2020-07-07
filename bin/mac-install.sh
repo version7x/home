4846cf1cf89689f884fa943040af3db43b8aa5c7
version7x <32801437+version7x@users.noreply.github.com>
2020-07-07 12:12:29 -0400

    updated order, comments.  added darktable

--- bin/mac-install.sh
+++ bin/mac-install.sh
@@ -24,16 +24,28 @@ ln -s home/.vimrc $HOME/.vimrc
 
 . ~/.bash_profile
 
-# Install pkgs
+# Software/Package Install
+
+# Python
 brew install python3
 pip3 install --upgrade pip --user
 pip3 install pipenv --user
 
+# Desktop Apps
+
 brew cask install google-backup-and-sync
 brew install brave-browser
+brew cask install darktable
+brew cask install microsoft-office
 
+#DevOps
 brew install terraform
 brew cask install docker
+# Visual Studio Code
+brew cask install visual-studio-code
+for EXT in 4ops.terraform ms-azuretools.vscode-docker ms-python.python; do
+  code --install-extension $EXT
+done
 
 # Install Mac App Store CLI
 # https://github.com/mas-cli/mas
@@ -42,16 +54,11 @@ brew install mas
 #mas signin --dialog version7x@mac.com
 mas signin --dialog version7x@mac.com'
 
-# Install magnet application from MAS
+# Mac App Store Installs
+# Magnet - workspace snapping
 mas install 441258766
-# Install remote desktop app
+# Install Microsoft Remote Desktop
 mas install 1295203466
+# Install CopyClip - Clipboard manager
+mas install 595191960
 
-# Visual Studio Code
-brew cask install visual-studio-code
-for EXT in 4ops.terraform ms-azuretools.vscode-docker ms-python.python; do
-  code --install-extension $EXT
-done
-
-# MS Office
-brew cask install microsoft-office
